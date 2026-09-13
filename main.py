88f13e746cb40b17ea1065a945e09b965d05724e
thearmadiliking <roi.steinitz@iasa-school.org.il>
2026-09-13 22:27:19 +0300

    new file:   main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+
+#loding screen
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip() 
+
+    
+#game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.flip() 
