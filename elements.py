5446ccb6fbb3d92bf75da2b42586f20c33d4078c
thearmadiliking <roi.steinitz@iasa-school.org.il>
2026-09-17 14:10:06 +0300

    do shit... i added the "IMPOSTER" and its basic form... its a rect. and i allso added a file format for making levels .lvl and a def to load it and format it and stringyfi/intyfi it so it will work

--- elements.py
+++ elements.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 class slider:
 
@@ -15,7 +16,7 @@ class slider:
         self.pyg = pygame
 
     def update(self):
-        if self.time <= 0:
+        if self.time >= 0:
             self.pyg.draw.rect(self.screen,(0,20,255),(self.x,self.y,self.l,self.h))
 
             self.last_time = time.time()
@@ -25,3 +26,26 @@ class slider:
             self.x = self.screen.get_width()/2 - self.l/2
         else:
             return("haha")
+
+class rect:
+    def __init__(self,e,rgb,d,imposter,pygame,screen):
+        self.e = e
+        self.d = d
+        self.rgb = rgb if imposter != e else (max(min(rgb[0]+random.randint(-self.d,self.d),255),0), max(min(rgb[1]+random.randint(-self.d,self.d),255),0), max(min(rgb[2]+random.randint(-self.d,self.d),255),0))
+        self.imposter = imposter
+        self.pygame = pygame
+        self.screen = screen
+        if self.e == 1:
+            self.x = 50
+        elif self.e == 2:
+            self.x = 110
+        elif self.e == 3:
+            self.x = 170
+        self.y = 50
+        self.l = 50
+        self.h = 100
+
+    def update(self):
+        self.pygame.draw.rect(self.screen,self.rgb,(self.x,self.y,self.l,self.h))
+        print(self.rgb)
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import elements
+import re
 pygame.init()
 
 width, height = 800, 600
@@ -13,6 +14,37 @@ font = pygame.font.SysFont(
     italic=True
 )
 text = font.render('Impster', True, (230,10,15))
+
+def intyfi(not_int,line):
+    return int(str(re.findall(not_int,line)[0]))
+
+def stringyfi(not_int,line):
+    try:
+        return str(re.findall(not_int,line)[0])
+    except:
+        return 0
+
+
+#level player
+level = []
+def load_level(lvl_nmb):
+    with open(f'levels/{lvl_nmb}.lvl', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            pice_time = intyfi(r'time-(\d)',line)
+            efect = intyfi(r'efect-(\d)',line)
+            anim = intyfi(r'anim-(\d)',line)
+            r = intyfi(r'r-(\d+)',line)
+            g = intyfi(r'g-(\d+)',line)
+            b = intyfi(r'b-(\d+)',line)
+            dificalty = intyfi(r'd-(\d+)',line)
+            e1 = stringyfi(r'e1-(\w)',line)
+            e2 = stringyfi(r'e2-(\w)',line)
+            e3 = stringyfi(r'e3-(\w)',line)
+
+            level.append([pice_time,efect,anim,r,g,b,dificalty,e1,e2,e3])
+load_level(1)
+
 
 #main menu loop
 running = True
@@ -29,7 +61,11 @@ while running:
 
 #game loop
 running = True
-slider = elements.slider(60,0.2,screen,pygame)
+slider = elements.slider(60,1,screen,pygame)
+e1_test = elements.rect(1,(0,30,255),65,2,pygame,screen)
+e2_test = elements.rect(2,(0,30,255),65,2,pygame,screen)
+e3_test = elements.rect(3,(0,30,255),65,2,pygame,screen)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,4 +73,7 @@ while running:
 
     screen.fill((0, 0, 0))
     slider.update()
+    e1_test.update()
+    e2_test.update()
+    e3_test.update()
     pygame.display.flip()
