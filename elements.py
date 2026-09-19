aa9935cf3747a262ec17c861d81c77147b950b36
thearmadiliking <roi.steinitz@iasa-school.org.il>
2026-09-19 16:36:55 +0300

    idk

--- elements.py
+++ elements.py
@@ -31,21 +31,27 @@ class rect:
     def __init__(self,e,rgb,d,imposter,pygame,screen):
         self.e = e
         self.d = d
+        trys = 0
         self.rgb = rgb if imposter != e else (max(min(rgb[0]+random.randint(-self.d,self.d),255),0), max(min(rgb[1]+random.randint(-self.d,self.d),255),0), max(min(rgb[2]+random.randint(-self.d,self.d),255),0))
+        while (rgb[0] - self.rgb[0]) + (rgb[1] - self.rgb[1]) + (rgb[2] - self.rgb[2]) <= d-5 and imposter == e:
+            print(f"try {trys}")
+            trys += 1
+            self.rgb = (max(min(rgb[0]+random.randint(-self.d,self.d),255),0), max(min(rgb[1]+random.randint(-self.d,self.d),255),0), max(min(rgb[2]+random.randint(-self.d,self.d),255),0))
+        print(f"final rgb: {self.rgb}")
         self.imposter = imposter
         self.pygame = pygame
         self.screen = screen
+        self.l = (screen.get_width()-30)/3 - 10
+        self.h = screen.get_height() - 150
         if self.e == 1:
-            self.x = 50
+            self.x = self.l * 0+ 20
         elif self.e == 2:
-            self.x = 110
+            self.x = self.l * 1 + 30
         elif self.e == 3:
-            self.x = 170
-        self.y = 50
-        self.l = 50
-        self.h = 100
+            self.x = self.l * 2 + 40
+        self.y = 100
+
 
     def update(self):
         self.pygame.draw.rect(self.screen,self.rgb,(self.x,self.y,self.l,self.h))
-        print(self.rgb)
 
